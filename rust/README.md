9c810451d0aa0f6383a6d58b68ed191f9e0f338f
guimcaballero <guim.caballero@gmail.com>
2021-02-25 21:46:33 +0100

    Rust implementation

---
+++ rust/README.md
@@ -0,0 +1,7 @@
+# HQ9+ implementation in Rust
+
+Run using:
+
+```
+cargo r ../tests/all.hq9p
+```
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,53 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return;
+    }
+
+    let filename = &args[1];
+    let program = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    execute_program(program);
+}
+
+fn execute_program(program: String) {
+    #[allow(unused_variables)]
+    let mut accumulator = 0;
+
+    for char in program.chars() {
+        match char {
+            'h' | 'H' => print!("Hello, world!"),
+            'q' | 'Q' => print!("{}", program),
+            '9' => print_99(),
+            '+' => accumulator += 1,
+            _ => {}
+        }
+    }
+}
+
+fn print_99() {
+    let mut count = 99;
+    while count > 0 {
+        println!(
+            "{} bottles of beer on the wall, {} bottles of beer.",
+            count, count
+        );
+        count -= 1;
+
+        if count != 0 {
+            println!(
+                "Take one down and pass it around, {} bottles of beer on the wall.",
+                count
+            );
+            println!("");
+        } else {
+            println!("Take one down and pass it around, no more bottles of beer on the wall.");
+            println!("");
+            println!("No more bottles of beer on the wall, no more bottles of beer.");
+            println!("Go to the store and buy some more, 99 bottles of beer on the wall.");
+        }
+    }
 }
